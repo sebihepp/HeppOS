8c64bc4d0142317a4a83472724cf0d49dab6f9cc
Sebihepp <sebihepp@googlemail.com>
2024-07-25 16:18:53 +0200

    [kernel] added cxxstub and console output for an initial test

---
+++ kernel/memcmp.c
@@ -0,0 +1,18 @@
+
+#include "cxxstub.h"
+
+uint32_t memcmp(const void *a, const void *b, size_t length)
+{
+	const uint8_t *_a = (const uint8_t*)a;
+	const uint8_t *_b = (const uint8_t*)b;
+	
+	for (size_t i = 0; i < length; i++)
+	{
+		if (_a[i] < _b[i]) {
+			return -1;
+		} else if (_a[i] > _b[i]) {
+			return 1; 
+		}
+	}
+	return 0;
+}
