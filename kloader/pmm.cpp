cee1d82b57c939a9216c74ca13c8a11ca6a5fab9
Sebihepp <sebihepp@googlemail.com>
2024-08-12 12:21:46 +0200

    [kloader/pmm] set every page to be blocked as first step of init

--- kloader/pmm.cpp
+++ kloader/pmm.cpp
@@ -6,5 +6,19 @@ uint8_t PMM::Bitmap[BITMAP_SIZE];
 
 retval_t PMM::Init(multiboot2_info_t *mbi) {
 
+	// First block every page
+	for (uint32_t i = 0; i < BITMAP_SIZE; i++) {
+		Bitmap[i] = 0xFF;
+	}
+	
+	// Free every page marked as free in MemoryMap
+	
+	
+	// Block every page marked as not free in MemoryMap
+	
+	
+	// Block additional pages not in MemoryMap (like framebuffer, BDA, EBDA, etc.)
+	
+	
 	return RETVAL_OK;
 }
