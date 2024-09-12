0747b9a1c9c671d356b022e386c712fee53c9910
Sebihepp <sebihepp@googlemail.com>
2024-09-12 11:39:49 +0200

    [kloader/cstub] changed return value of memcmp() from uint to int

--- kloader/cstub.h
+++ kloader/cstub.h
@@ -10,7 +10,7 @@
 extern "C" {
 #endif
 
-uint32_t memcmp(const void*, const void*, size_t);
+int32_t memcmp(const void*, const void*, size_t);
 void* memcpy(void*, const void*, size_t);
 void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);
