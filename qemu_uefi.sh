ad2beb623d6f4368ce39e361b51c03eea5fde641
Sebihepp <sebihepp@googlemail.com>
2024-11-07 09:52:58 +0100

    [general] qemu display library changed from sdl to gtk

--- qemu_uefi.sh
+++ qemu_uefi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-qemu-system-x86_64 -cpu EPYC -m 8192 -display sdl \
+qemu-system-x86_64 -cpu Skylake-Client-v1 -m 8192  \
 	-bios /usr/share/ovmf/OVMF.fd \
 	-drive if=none,id=stick,format=raw,file=HeppOS.img \
 	-device nec-usb-xhci,id=xhci \
