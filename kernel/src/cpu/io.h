6609e65c2bbd26e94f05a86c0bf750843bd3baf2
Sebihepp <sebihepp@googlemail.com>
2024-11-07 10:21:47 +0100

    [kernel] fixed asm input in io.h

--- kernel/src/cpu/io.h
+++ kernel/src/cpu/io.h
@@ -8,10 +8,11 @@
 inline uint8_t inb(uint16_t pPort) {
 	uint8_t _value = 0;
 	asm volatile (
+		"mov %1, %%dx;\n"
 		"inb %%dx, %%al;\n"
 		: "=a" (_value)
-		: "d" (pPort)
-		:
+		: "m" (pPort)
+		: "dx"
 	);
 	return _value;
 }
@@ -19,10 +20,11 @@ inline uint8_t inb(uint16_t pPort) {
 inline uint16_t inw(uint16_t pPort) {
 	uint16_t _value = 0;
 	asm volatile (
+		"mov %1, %%dx;\n"
 		"inw %%dx, %%ax;\n"
 		: "=a" (_value)
-		: "d" (pPort)
-		:
+		: "m" (pPort)
+		: "dx"
 	);
 	return _value;
 }
@@ -30,10 +32,11 @@ inline uint16_t inw(uint16_t pPort) {
 inline uint32_t inl(uint16_t pPort) {
 	uint32_t _value = 0;
 	asm volatile (
+		"mov %1, %%dx;\n"
 		"inl %%dx, %%eax;\n"
 		: "=a" (_value)
-		: "d" (pPort)
-		:
+		: "m" (pPort)
+		: "dx"
 	);
 	return _value;	
 }
@@ -41,28 +44,34 @@ inline uint32_t inl(uint16_t pPort) {
 
 inline void outb(uint16_t pPort, uint8_t pValue) {
 	asm volatile (
+		"mov %0, %%al;\n"
+		"mov %1, %%dx;\n"
 		"outb %%al, %%dx;\n"
 		:
-		: "a" (pValue), "d" (pPort)
-		:
+		: "m" (pValue), "m" (pPort)
+		: "al", "dx"
 	);
 }
 
 inline void outw(uint16_t pPort, uint16_t pValue) {
-		asm volatile (
+	asm volatile (
+		"mov %0, %%ax;\n"
+		"mov %1, %%dx;\n"
 		"outw %%ax, %%dx;\n"
 		:
-		: "a" (pValue), "d" (pPort)
-		:
+		: "m" (pValue), "m" (pPort)
+		: "ax", "dx"
 	);
 }
 
 inline void outl(uint16_t pPort, uint32_t pValue) {
-		asm volatile (
+	asm volatile (
+		"mov %0, %%eax;\n"
+		"mov %1, %%dx;\n"
 		"outl %%eax, %%dx;\n"
 		:
-		: "a" (pValue), "d" (pPort)
-		:
+		: "m" (pValue), "m" (pPort)
+		: "eax", "dx"
 	);
 }
 
