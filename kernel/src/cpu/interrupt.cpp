f8f0039890b7f6da222b64854acf8d6f5a3db26d
Sebihepp <sebihepp@googlemail.com>
2024-11-18 12:19:17 +0100

    [kernel/msr] Overloaded function ReadMSR and WriteMSR to also support direct 64bit read/write

--- kernel/src/cpu/interrupt.cpp
+++ kernel/src/cpu/interrupt.cpp
@@ -135,20 +135,14 @@ extern "C" void ISRHandler(uint64_t pInt, CPUState_t *pState) {
 extern "C" void ExceptionHandler(uint64_t pInt, CPUState_t *pState) {
 	
 	char _TempString[32];
-	uint64_t _cr0 = 0;
-	uint64_t _cr2 = 0;
-	uint64_t _cr3 = 0;
-	uint64_t _cr4 = 0;
-	uint64_t _cr8 = 0;
-	uint64_t _dr6 = 0;
-	uint64_t _dr7 = 0;
-	union {
-		uint64_t u64;
-		struct {
-			uint32_t l32;
-			uint32_t h32;
-		}__attribute__((packed));
-	}__attribute__((packed)) _efer;
+	uint64_t _CR0 = 0;
+	uint64_t _CR2 = 0;
+	uint64_t _CR3 = 0;
+	uint64_t _CR4 = 0;
+	uint64_t _CR8 = 0;
+	uint64_t _DR6 = 0;
+	uint64_t _DR7 = 0;
+	uint64_t _EFER = 0;
 	
 	asm volatile (
 		"mov %%cr0, %0;\n"
@@ -158,10 +152,10 @@ extern "C" void ExceptionHandler(uint64_t pInt, CPUState_t *pState) {
 		"mov %%cr8, %4;\n"
 		"mov %%dr6, %5;\n"
 		"mov %%dr7, %6;\n"
-		: "=r" (_cr0), "=r" (_cr2), "=r" (_cr3), "=r" (_cr4), "=r" (_cr8), "=r" (_dr6), "=r" (_dr7)
+		: "=r" (_CR0), "=r" (_CR2), "=r" (_CR3), "=r" (_CR4), "=r" (_CR8), "=r" (_DR6), "=r" (_DR7)
 	);
 	
-	ReadMSR(MSR_EFER, &(_efer.l32), &(_efer.h32));
+	ReadMSR(MSR_EFER, &_EFER);
 	
 	Interrupt::mInterruptCount[pInt] += 1;
 	
@@ -261,32 +255,32 @@ extern "C" void ExceptionHandler(uint64_t pInt, CPUState_t *pState) {
 
 
 	Console::Print("CR0=0x");
-	Console::Print(htoa(_cr0, _TempString));
+	Console::Print(htoa(_CR0, _TempString));
 	Console::Print(" \tCR2=0x");
-	Console::Print(htoa(_cr2, _TempString));
+	Console::Print(htoa(_CR2, _TempString));
 	Console::Print("\n");
 
 	Console::Print("CR3=0x");
-	Console::Print(htoa(_cr3, _TempString));
+	Console::Print(htoa(_CR3, _TempString));
 	Console::Print(" \tCR4=0x");
-	Console::Print(htoa(_cr4, _TempString));
+	Console::Print(htoa(_CR4, _TempString));
 	Console::Print("\n");
 	Console::Print("\n");
 
 	Console::Print("CR8=0x");
-	Console::Print(htoa(_cr8, _TempString));
+	Console::Print(htoa(_CR8, _TempString));
 	Console::Print("\n");
 	Console::Print("\n");
 	
 	Console::Print("EFER=0x");
-	Console::Print(htoa(_efer.u64, _TempString));
+	Console::Print(htoa(_EFER, _TempString));
 	Console::Print("\n");
 	Console::Print("\n");
 	
 	Console::Print("DR6=0x");
-	Console::Print(htoa(_dr6, _TempString));
+	Console::Print(htoa(_DR6, _TempString));
 	Console::Print(" \tDR7=0x");
-	Console::Print(htoa(_dr7, _TempString));
+	Console::Print(htoa(_DR7, _TempString));
 	Console::Print("\n");
 	Console::Print("\n");
 	
