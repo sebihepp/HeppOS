65a1e5230b7860dbbb554da946d32288878c7fa4
Sebihepp <sebihepp@googlemail.com>
2025-06-26 17:02:11 +0200

    [general] added license info on top of each sourcecode file

--- kernel/src/video/stdfont.cpp
+++ kernel/src/video/stdfont.cpp
@@ -1,3 +1,20 @@
+/* 
+    Copyright (C) 2025  Sebastian Hepp aka sebihepp
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 #include <stddef.h>
 #include <stdint.h>
@@ -8,7 +25,7 @@ extern "C" {
 
 #define FONTDATAMAX 16 * 256
 
-const volatile uint8_t STDFont[FONTDATAMAX] = {
+const volatile uint8_t gSTDFont[FONTDATAMAX] = {
 
 	/* 0 0x00 '^@' */
 	0x00, /* 00000000 */
